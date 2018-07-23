19df208d82adcf177df357f2b05e8cfe41655ce0
rkw <rkw@touk.pl>
2018-07-23 15:25:09 +0200

    Adjust max element size for Firefox browser

--- source/Grid/utils/maxElementSize.js
+++ source/Grid/utils/maxElementSize.js
@@ -2,15 +2,21 @@
 
 const DEFAULT_MAX_ELEMENT_SIZE = 1500000;
 const CHROME_MAX_ELEMENT_SIZE = 1.67771e7;
+const FIREFOX_MAX_ELEMENT_SIZE = 1.34217e7;
 
 const isBrowser = () => typeof window !== 'undefined';
 
 const isChrome = () => !!window.chrome && !!window.chrome.webstore;
 
+const isFirefox = () => typeof InstallTrigger !== 'undefined';
+
 export const getMaxElementSize = (): number => {
   if (isBrowser()) {
     if (isChrome()) {
       return CHROME_MAX_ELEMENT_SIZE;
+    }
+    if (isFirefox()) {
+      return FIREFOX_MAX_ELEMENT_SIZE;
     }
   }
   return DEFAULT_MAX_ELEMENT_SIZE;
